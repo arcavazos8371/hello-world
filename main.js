d3654f67e42485ab879f4fbc5a9bd0632b93ad00
Albert Cavazos <arcavazos8371@gmail.com>
2021-05-19 15:43:48 -0500

    First working page with simple javascript

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/code.png') {
+      myImage.setAttribute('src','images/HTML.png');
+    } else {
+      myImage.setAttribute('src','images/code.png');
+    }
+}
